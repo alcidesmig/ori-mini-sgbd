a8b9c8a553194a3a19fda10a1179d32d80f8358b
Matheus Victorello <matheusvictorello@gmail.com>
2019-08-23 01:17:24 -0300

    Essa mensagem é realmente importante?

---
+++ defines.c
@@ -0,0 +1,9 @@
+const char STR[4] = "STR";
+const char BIN[4] = "BIN";
+const char INT[4] = "INT";
+const char FLT[4] = "FLT";
+
+const char STR_REP = 'S';
+const char BIN_REP = 'B';
+const char INT_REP = 'I';
+const char FLT_REP = 'F';
